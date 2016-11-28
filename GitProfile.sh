d0f8f320452fa322d97950ba05ee3cdaa99bd10b
Ryder Stancescu <hstances@gmail.com>
2016-11-28 12:47:46 -0800

    ":bug:Fixed message being cutoff."

--- GitProfile.sh
+++ GitProfile.sh
@@ -19,90 +19,90 @@ new=":bulb:";
 function gitCommit
 {
 	# $1 = Argument 1
-	git commit -m \""$1"\"
+	git commit -m $1
 }
 
 # ShortHand for Commit with WIP Emoji
 function gitc
 {
-	commit="$wip$1";
-	gitCommit $commit;
+	commit=$wip$1;
+	gitCommit "$commit";
 }
 # ShortHand for Commit with WIP Emoji
 function gitc_Bug
 {
 	commit="$bug$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Art
 {
 	commit="$art$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Log
 {
 	commit="$log$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Mute
 {
 	commit="$mute$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Doc
 {
 	commit="$doc$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Doc
 {
 	commit="$opt$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 # Optimize
 function gitc_Opt
 {
 	commit="$opt$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Dist
 {
 	commit="$dist$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Up
 {
 	commit="$up$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Down
 {
 	commit="$down$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Test
 {
 	commit="$test$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Add
 {
 	commit="$add$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Test
 {
 	commit="$rmv$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 function gitc_Sec
 {
 	commit="$sec$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
 
 function gitc_New
 {
 	commit="$new$1";
-	gitCommit $commit;
+	gitCommit "$commit";
 }
